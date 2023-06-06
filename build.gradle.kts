3c53efefb976003906580e8196809f61c1c896b8
Lukáš Kopecký <lukas.kopecky.494@gmail.com>
2023-06-06 15:01:50 +0200

    add versions to gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,7 @@ repositories {
     mavenCentral()
 }
 val graphqlKotlinVersion: String by project
+val extendScalarsVersion: String by project
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
@@ -23,8 +24,8 @@ dependencies {
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-    implementation("com.expediagroup:graphql-kotlin-spring-server:6.4.0")
-    implementation("com.graphql-java:graphql-java-extended-scalars:20.0")
+    implementation("com.expediagroup:graphql-kotlin-spring-server:$graphqlKotlinVersion")
+    implementation("com.graphql-java:graphql-java-extended-scalars:$extendScalarsVersion")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
 }
